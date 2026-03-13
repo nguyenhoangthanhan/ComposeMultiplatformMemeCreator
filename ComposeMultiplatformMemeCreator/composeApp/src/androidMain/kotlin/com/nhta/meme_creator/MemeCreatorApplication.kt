264bf8b5782637746dd9a1bab4432ea5a68ed75a
NguyenHoangThanhAn <skyannguyenhoangthanh@gmail.com>
2026-03-14 00:03:39 +0700

    add fonts, themes

---
+++ ComposeMultiplatformMemeCreator/composeApp/src/androidMain/kotlin/com/nhta/meme_creator/MemeCreatorApplication.kt
@@ -0,0 +1,15 @@
+package com.nhta.meme_creator
+
+import android.app.Application
+//import com.plcoding.cmp_memecreator.di.initKoin
+//import org.koin.android.ext.koin.androidContext
+
+class MemeCreatorApplication: Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+//        initKoin {
+//            androidContext(this@MemeCreatorApplication)
+//        }
+    }
+}
