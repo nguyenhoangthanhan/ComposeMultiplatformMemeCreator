69b47cc6bf2ecd2d25db3ad2205d27cc0f0bf22e
NguyenHoangThanhAn <skyannguyenhoangthanh@gmail.com>
2026-03-30 23:30:29 +0700

    add object meme editor

--- ComposeMultiplatformMemeCreator/composeApp/src/commonMain/kotlin/com/nhta/meme_creator/App.kt
+++ ComposeMultiplatformMemeCreator/composeApp/src/commonMain/kotlin/com/nhta/meme_creator/App.kt
@@ -23,7 +23,9 @@ import composemultiplatformmemecreator.composeapp.generated.resources.compose_mu
 @Preview
 fun App() {
     MaterialTheme {
+
         var showContent by remember { mutableStateOf(false) }
+
         Column(
             modifier = Modifier
                 .background(MaterialTheme.colorScheme.primaryContainer)
--- ComposeMultiplatformMemeCreator/composeApp/src/commonMain/kotlin/com/nhta/meme_creator/meme_editor/data/CacheStorageStrategy.kt
+++ ComposeMultiplatformMemeCreator/composeApp/src/commonMain/kotlin/com/nhta/meme_creator/meme_editor/data/CacheStorageStrategy.kt
@@ -2,7 +2,6 @@ package com.nhta.meme_creator.meme_editor.data
 
 import com.nhta.meme_creator.meme_editor.domain.SaveToStorageStrategy
 
-
 expect class CacheStorageStrategy: SaveToStorageStrategy {
     override fun getFilePath(fileName: String): String
 }
